2c7d47a6bc48ac5eaa795a22ba389c90bdd5ca3f
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-10-17 11:00:53 +0700

    menambahkan Spacer modifier

--- app/src/main/java/com/example/prak2pam/Tataletak.kt
+++ app/src/main/java/com/example/prak2pam/Tataletak.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalProvider
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -142,4 +143,13 @@ fun TataletakRowColumn(modifier: Modifier) {
                 }
             }
             Spacer(modifier = Modifier.height(height = 10.dp))
+            Box(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(height = 300.dp)
+                    .background(color = Color.Cyan),
+                contentAlignment = Alignment.Center
+            ){
+
+            }
 
